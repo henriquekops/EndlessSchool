d591aadd5efb422cfaf2593c69ee9dca7f2e2c64
henrique.kops <henrique.kops@acad.pucrs.br>
2022-05-15 19:29:30 -0300

    add visibility notifier

--- scripts/Projectile.gd
+++ scripts/Projectile.gd
@@ -8,7 +8,13 @@ export var speed = 700
 
 onready var direction: int =  Direction.UP
 
+var delay = 5
+
 func _physics_process(delta):
+	delay -= 1
+	if delay == 0:
+		print("bla")
+		delay = 5
 	match direction:
 		Direction.UP:
 			position -= transform.y * speed * delta
@@ -18,3 +24,7 @@ func _physics_process(delta):
 			position -= transform.x * speed * delta
 		Direction.RIGHT:
 			position += transform.x * speed * delta
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
