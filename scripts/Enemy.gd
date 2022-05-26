fa2358981473dad496c491aa3b5cc1f74997e995
igorcardonaalves <igorcardonaalves@hotmail.com>
2022-05-26 11:48:05 +0000

    adicionando arte do iminigo

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -14,11 +14,43 @@ onready var raycast: RayCast2D = $RayCast2D
 onready var screen_size: Vector2 = get_viewport().size
 onready var raycast_coordinates: Array = raycast_coordinates()
 
+onready var animatedSprite: AnimatedSprite = $AnimatedSprite;
+
+func verificaAnimacao(player):
+	var vertical = 0;
+	var horizontal = 0;
+	if player.position.y > position.y:
+		vertical = 1;
+	else:
+		vertical = -1;
+		
+	if player.position.x > position.x:
+		horizontal = 1;
+	else:
+		horizontal = -1;
+	
+	var animacaoAtual = "down";
+	if vertical == -1:
+		animacaoAtual = "up";
+	elif horizontal == 1:
+		animacaoAtual = "right";
+	elif vertical == 1:
+		animacaoAtual = "down";
+	else:
+		animacaoAtual = "left";
+	animatedSprite.play(animacaoAtual);
+	pass
+
+
+
 func _physics_process(delta: float) -> void:
 	for ray_coordinate in raycast_coordinates():
 		raycast.cast_to = ray_coordinate
 		raycast.force_raycast_update()
 		if raycast.is_colliding() and raycast.get_collider() is Player:
+			
+			verificaAnimacao( raycast.get_collider());
+			
 			target = raycast.get_collision_point()
 			var dir = (target - global_position).normalized()
 			position.x = clamp(position.x, 50, screen_size.x-50)
