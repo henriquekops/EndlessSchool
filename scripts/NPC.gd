f7d8050daac0f71f116ff8c447a317603ff78094
GabrielCastroSilva <gabriel.silva.010@acad.pucrs.br>
2022-06-02 10:41:31 -0300

    Fixed NPC hitbox

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 func _input(event):
-	if event.is_action_pressed("Interact") and len(get_overlapping_bodies()) > 0:
+	
+	print(len(get_overlapping_bodies()))
+	if event.is_action_pressed("Interact") and len(get_overlapping_bodies()) > 1:
 		find_and_use_dialogue()
 		
 func find_and_use_dialogue():
-	var dialogue_player = get_node("../DialogueBox")
+	var dialogue_player = get_parent().get_node("DialogueBox")
 	if dialogue_player:
 		dialogue_player.play()
