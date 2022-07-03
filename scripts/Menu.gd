2f68d0bc0ca419fde4cdee236a9b2d61049afed2
GabrielCastroSilva <gabriel.silva.010@acad.pucrs.br>
2022-07-03 18:29:55 -0300

    Added MainMenu and Instructions screen

---
+++ scripts/Menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready() -> void:
+	# Trocar o nome da cena principal!
+	PlayerSingleton.visible = false
+	ScoreSingleton.get_child(0).hide()
+	HudSingleton.get_child(0).hide()
+
+func _on_Button_pressed() -> void:
+	# Trocar o nome da cena principal!
+	PlayerSingleton.visible = true
+	ScoreSingleton.get_child(0).show()
+	HudSingleton.get_child(0).show()
+	get_tree().change_scene("res://scenes/School.tscn")
+
+
+func _on_Instructions_pressed():
+	get_tree().change_scene("res://scenes/Instructions.tscn")
